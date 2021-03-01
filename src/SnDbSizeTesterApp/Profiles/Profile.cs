fe7bf3457318506604a657f68e58d2bd600e8608
kavics <gyeby5@gmail.com>
2021-03-01 03:50:59 +0100

    Uploader profile. (#1)

--- src/SnDbSizeTesterApp/Profiles/Profile.cs
+++ src/SnDbSizeTesterApp/Profiles/Profile.cs
@@ -1,13 +1,14 @@
 ﻿using System;
 using System.Threading;
 using System.Threading.Tasks;
+using SenseNet.Client;
 
 namespace SnDbSizeTesterApp.Profiles
 {
     public abstract class Profile
     {
-        public bool Recurring { get; set; } = true;
-        public int WaitMilliseconds { get; set; } = 1250;
+        public bool Recurring { get; set; } = false;
+        public int WaitMilliseconds { get; set; } = 5000;
 
         public abstract string Name { get; }
         public abstract Task Action(CancellationToken cancellation);
@@ -16,6 +17,28 @@ namespace SnDbSizeTesterApp.Profiles
         protected void Print(string text)
         {
             _printAction(text);
+        }
+
+private bool _startup = true;
+        protected async Task<Content> GetTestFolderAsync()
+        {
+            var uploadRootPath = "/Root/UploadTests";
+
+            var uploadFolder = await Content.LoadAsync(uploadRootPath).ConfigureAwait(false);
+if (uploadFolder != null && _startup)
+{
+    await uploadFolder.DeleteAsync();
+    uploadFolder = null;
+    _startup = false;
+}
+            if (uploadFolder == null)
+            {
+                uploadFolder = Content.CreateNew("/Root", "SystemFolder", "UploadTests");
+                await uploadFolder.SaveAsync().ConfigureAwait(false);
+            }
+
+
+            return uploadFolder;
         }
     }
 }
--- src/SnDbSizeTesterApp/Profiles/UploaderProfile.cs
+++ src/SnDbSizeTesterApp/Profiles/UploaderProfile.cs
@@ -1,15 +1,37 @@
-﻿using System.Threading;
+﻿using System;
+using System.IO;
+using System.Threading;
 using System.Threading.Tasks;
+using SenseNet.Client;
 
 namespace SnDbSizeTesterApp.Profiles
 {
     public class UploaderProfile : Profile
     {
         public override string Name => "Uploader";
-        public override Task Action(CancellationToken cancellation)
+
+        private readonly int size = 1 * 512 * 1024;
+        public override async Task Action(CancellationToken cancellation)
         {
+            Content root = await base.GetTestFolderAsync().ConfigureAwait(false);
+
+            var chars = "Test file content. ".ToCharArray();
+
+            var buffer = new byte[size];
+            for (int i = 0; i < buffer.Length; i++)
+                buffer[i] = Convert.ToByte(chars[i % chars.Length]);
+
+            var stream = new MemoryStream(buffer);
+            try
+            {
+                await Content.UploadAsync(root.Id, Guid.NewGuid().ToString(), stream).ConfigureAwait(false);
+            }
+            catch (Exception e)
+            {
+                Print(e.ToString());
+            }
+
             Print("U");
-            return Task.CompletedTask;
         }
 
     }
